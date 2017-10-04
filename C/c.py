2e4dcaa8ff5cb6a3bca03ef0825de394fa841675
AdamBeziou <31672539+AdamBeziou@users.noreply.github.com>
2017-10-03 21:17:09 -0600

    Create c.py

---
+++ C/c.py
@@ -0,0 +1,19 @@
+import sys
+
+for i in sys.stdin:
+  coord = i.splitlines()
+	coord_list = open(coord[0], 'r').read().splitlines()
+	coord_list = [int(x) for x in coord_list]
+	x = coord_list[0]
+	y = coord_list[1]
+	if x > 0:
+		if y > 0:
+			print("1")
+		else:
+			print("4")
+	else:
+		if y > 0:
+			print("2")
+		else:
+			print("3")
+	break
